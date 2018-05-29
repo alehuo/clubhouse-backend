e120107e4d9d60e1bf0555cc6126c6639d290253
alehuo <aleksi.huotala@helsinki.fi>
2018-05-29 14:41:12 +0300

    Preparing application for deployment to Heroku.

--- doc/working_hours.md
+++ doc/working_hours.md
@@ -36,6 +36,6 @@
 | 9.5.2018  | 1h    | Hook up permission system to routes & improve tests for Student union controller                                                                                                                                                                                                           |
 | 12.5.2018 | 5h    | Moved from SQLite to MySQL. Fixed tests, migrations, seeds and Knexfile as SQLite was initially used in the development process. Created WatchFilter to fix the GET route return schema. Added instructions to README.md on how to start a MySQL server. Created database creation script. |
 | 20.5.2018 | 0.5h  | Created additional routes for checking watches and updated REST files.                                                                                                                                                                                                                     |
-| 29.5.2018 | 1h    | Added list of permissions to be returned from the permission route. Improved permission seed.                                                                                                                                                                                              |
+| 29.5.2018 | 1.5h  | Added list of permissions to be returned from the permission route. Improved permission seed. Preparing application for deployment to Heroku.                                                                                                                                              |
 
-Total: 58.5 h
+Total: 59.0 h
--- knexfile.js
+++ knexfile.js
@@ -1,81 +1,81 @@
 // Update with your config settings.
 require("dotenv").config();
 
-module.exports = {
+const db_configuration = {
   development: {
     client: "mysql2",
-    connection: {
-      host: process.env.MYSQL_HOST,
-      port: process.env.MYSQL_PORT,
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DB_NAME + "_dev"
-    },
+    connection: process.env.MYSQL_CONNECTION_STRING
+      ? process.env.MYSQL_CONNECTION_STRING
+      : "mysql://" +
+        process.env.MYSQL_USER +
+        ":" +
+        process.env.MYSQL_PASSWORD +
+        "@" +
+        process.env.MYSQL_HOST +
+        ":" +
+        process.env.MYSQL_PORT +
+        "/" +
+        process.env.MYSQL_DB_NAME +
+        "_dev?charset=utf8&timezone=UTC",
     seeds: {
       directory: "./seeds/dev"
     },
     pool: {
       min: 2,
-      max: 10,
-      afterCreate: function(conn, cb) {
-        conn.query('SET sql_mode="NO_ENGINE_SUBSTITUTION";', function(err) {
-          cb(err, conn);
-        });
-      }
+      max: 10
     }
   },
 
   test: {
     client: "mysql2",
-    connection: {
-      host: process.env.MYSQL_HOST,
-      port: process.env.MYSQL_PORT,
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DB_NAME + "_test"
-    },
+    connection: process.env.MYSQL_CONNECTION_STRING
+      ? process.env.MYSQL_CONNECTION_STRING
+      : "mysql://" +
+        process.env.MYSQL_USER +
+        ":" +
+        process.env.MYSQL_PASSWORD +
+        "@" +
+        process.env.MYSQL_HOST +
+        ":" +
+        process.env.MYSQL_PORT +
+        "/" +
+        process.env.MYSQL_DB_NAME +
+        "_test?charset=utf8&timezone=UTC",
     seeds: {
       directory: "./seeds/test"
     },
     pool: {
       min: 2,
-      max: 10,
-      afterCreate: function(conn, cb) {
-        conn.query('SET sql_mode="NO_ENGINE_SUBSTITUTION";', function(err) {
-          cb(err, conn);
-        });
-      }
+      max: 10
     }
   },
 
   production: {
     client: "mysql2",
-    connection: {
-      host: process.env.MYSQL_HOST,
-      port: process.env.MYSQL_PORT,
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DB_NAME
-    },
+    connection: process.env.MYSQL_CONNECTION_STRING
+      ? process.env.MYSQL_CONNECTION_STRING
+      : "mysql://" +
+        process.env.MYSQL_USER +
+        ":" +
+        process.env.MYSQL_PASSWORD +
+        "@" +
+        process.env.MYSQL_HOST +
+        ":" +
+        process.env.MYSQL_PORT +
+        "/" +
+        process.env.MYSQL_DB_NAME +
+        "?charset=utf8&timezone=UTC",
     seeds: {
       directory: "./seeds/production"
     },
     pool: {
       min: 2,
-      max: 10,
-      afterCreate: function(conn, cb) {
-        conn.query('SET sql_mode="NO_ENGINE_SUBSTITUTION";', function(err) {
-          cb(err, conn);
-        });
-      }
+      max: 10
     },
     migrations: {
       tableName: "knex_migrations"
-    },
-    afterCreate: function(conn, cb) {
-      conn.query('SET sql_mode="NO_ENGINE_SUBSTITUTION";', function(err) {
-        cb(err, conn);
-      });
     }
   }
 };
+
+module.exports = db_configuration;
