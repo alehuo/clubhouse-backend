4ea73a557a19ce7560edc830e55ca995636ea3b4
alehuo <aleksi.huotala@helsinki.fi>
2018-08-30 23:03:02 +0300

    Modify test behavior

--- test/middleware/PermissionMiddleware.test.ts
+++ test/middleware/PermissionMiddleware.test.ts
@@ -82,7 +82,7 @@ describe("PermissionMiddleware", () => {
         permissions: Math.pow(2, 4)
       }).replace("Bearer ", "")
     );
-    
+
     const request: httpMocks.MockRequest<any> = httpMocks.createRequest({
       method: "GET",
       url: "/user/42",
