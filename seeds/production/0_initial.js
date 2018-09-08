b616da26cf61a0de1b1602fe4d087e55fd90e0b1
alehuo <aleksi.huotala@helsinki.fi>
2018-09-08 19:24:15 +0300

    Modify seed

--- seeds/production/0_initial.js
+++ seeds/production/0_initial.js
@@ -1,29 +1,3 @@
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex("newsposts")
-    .del()
-    .then(function() {
-      // Inserts seed entries
-      return knex("watches")
-        .del()
-        .then(function() {
-          return knex("calendarEvents")
-            .del()
-            .then(function() {
-              return knex("locations")
-                .del()
-                .then(function() {
-                  return knex("users")
-                    .del()
-                    .then(function() {
-                      return knex("permissions")
-                        .del()
-                        .then(function() {
-                          return knex("studentUnions").del();
-                        });
-                    });
-                });
-            });
-        });
-    });
+  // Production seed doesn't do anything
 };
--- seeds/production/3_users.js
+++ seeds/production/3_users.js
@@ -1,18 +1,3 @@
-const bcrypt = require("bcrypt");
-var salt = bcrypt.genSaltSync(10);
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex("users").then(function() {
-    // Inserts seed entries
-    return knex("users").insert([
-      {
-        email: "testuser@email.com",
-        firstName: "Test",
-        lastName: "User",
-        permissions: 67108863,
-        password: bcrypt.hashSync("testuser", salt)
-      }
-    ]);
-  });
+  // Production seed doesn't do anything
 };
